70ce42d1582575eed940b1e7d71cd61e774c7bb7
Digvijay1209 <digvijaysingh1209@gmail.com>
2024-06-18 16:09:41 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        appDir: true, // Ensure your version of Next.js supports this
+        serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+        domains: ['lh3.googleusercontent.com'],
+    },
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        }
+        return config
+    }
+}
 
 export default nextConfig;
